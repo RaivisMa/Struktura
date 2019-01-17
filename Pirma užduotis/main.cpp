400b06a567274760a3f74f9b11cd3701b7987386
Raiva <Raiva@DESKTOP-09SHT5O>
2019-01-17 18:24:06 +0200

    update
    update

---
+++ Pirma užduotis/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{int n;
+    char name[22] = {82,97,105,118,121,100,97,115,32,77,97,116,101,105,107,97};
+	cout << name << endl;
+    return 0;
+}
